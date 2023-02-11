a1d3a675650e6d1c7760aa2489d5df011a56be3d
Mohamed Mohamed <63976154+Mohamed-N-Mohamed@users.noreply.github.com>
2023-02-11 17:57:11 +0000

    added showcase and fixed navbar with overlay

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,20 @@
 import React from "react";
+import Showcase from "./Showcase";
 
 const Navbar = () => {
   return (
-    <div className='navbar p-12 md:p-8'>
-      <ul className='flex justify-center items-center gap-4 md:justify-start'>
-        <li className='text-xl'>Home</li>
-        <li className='text-xl'>Champions</li>
-        <li className='text-xl'>Contact</li>
-      </ul>
-    </div>
+    <>
+      <header className='navbar relative text-white  h-[500px]'>
+        <div className='navbar-overlay p-12 md:p-8'>
+          <ul className='flex justify-center items-center gap-4 md:justify-start'>
+            <li className='text-xl'>Home</li>
+            <li className='text-xl'>Champions</li>
+            <li className='text-xl'>Contact</li>
+          </ul>
+          <Showcase />
+        </div>
+      </header>
+    </>
   );
 };
 
---
+++ src/components/Showcase.js
@@ -0,0 +1,18 @@
+import React from "react";
+import wallpaper_desktop from "../images/wallpaper-desktop.jpg";
+
+const Showcase = () => {
+  return (
+    <div className='showcase-text py-12'>
+      <h1 className='text-xl text-center md:text-2xl  py-4'>
+        League of champions
+      </h1>
+      <p className='px-4 text-center text-lg'>
+        Here you can search for any league of legends champions and find out
+        more information
+      </p>
+    </div>
+  );
+};
+
+export default Showcase;

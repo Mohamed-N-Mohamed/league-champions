74b59b250966bd7f046a503670a8cbb2ce01cbf8
Mohamed Mohamed <63976154+Mohamed-N-Mohamed@users.noreply.github.com>
2023-02-19 17:07:27 +0000

    can fetch champions but exceeded api limit

---
+++ src/components/Champion.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Champion = ({ champions }) => {
+  console.log(typeof champions)
+  return (
+    <div>
+      {/* {champions.length === 0 &&(
+      <div className="">Error No champions fetched</div>
+    )} */}
+
+
+
+     
+    </div>
+  );
+};
+
+export default Champion;
--- src/pages/Champions.js
+++ src/pages/Champions.js
@@ -1,7 +1,57 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import Champion from "../components/Champion";
 
 const Champions = () => {
-  return <div>This is working</div>;
+  const [champions, setChampions] = useState([]);
+
+  console.log(champions);
+  useEffect(() => {
+    fetchChampions();
+  }, []);
+
+  const fetchChampions = async () => {
+    const options = {
+      method: "GET",
+      url: "https://league-of-legends-champions.p.rapidapi.com/champions/en-gb",
+      params: { page: "0", size: "10", role: "fighter" },
+      headers: {
+        "X-RapidAPI-Key": `${process.env.REACT_APP_API_KEY}`,
+        "X-RapidAPI-Host": "league-of-legends-api1.p.rapidapi.com",
+      },
+    };
+
+    const response = await fetch(
+      "https://league-of-legends-api1.p.rapidapi.com/champions",
+      options
+    );
+    const data = await response.json();
+
+    //hold temporary champions
+    let c = [];
+
+    //loop through objects and push into a new array
+    for (const property in data) {
+      c.push(data[property]);
+    }
+    //get first 10 champions
+    let slicedArray = c.slice(0, 10);
+
+    //set champions
+    setChampions(slicedArray);
+    console.log(champions);
+
+  };
+
+  return (
+    <div className='championn p-4'>
+      <h2 className='text-center uppercase text-2xl'>
+        League of Legends ChampionsS
+      </h2>
+      <div className='show-champions'>
+        <Champion champions={champions} />
+      </div>
+    </div>
+  );
 };
 
 export default Champions;
